e1c9cf68885fa85cb82782126cc3f09f3db0a26c
Bhargav0025 <simplebhargav81@gmail.com>
2025-05-03 10:58:11 +0530

    Create GroceryApp.java

---
+++ src/main/java/com/example/groceryapp/GroceryApp.java
@@ -0,0 +1,11 @@
+package com.example.groceryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryApp.class, args);
+    }
+}
